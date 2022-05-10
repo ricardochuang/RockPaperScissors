24380bcb7dbb926ea04f4649df4aee3696a09ff5
leon-kwy <61376272+leon-kwy@users.noreply.github.com>
2022-05-10 02:20:46 -0400

    1

--- truffle-config.js
+++ truffle-config.js
@@ -14,5 +14,11 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
+
+    compilers: {
+      solc: {
+        version: "0.8.0",
+      },
+    },
   }
 };
