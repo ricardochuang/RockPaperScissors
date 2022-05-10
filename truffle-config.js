7969057fc31f8dbab54ebe9ce2a2bf3ff75db8f8
leon-kwy <61376272+leon-kwy@users.noreply.github.com>
2022-05-10 02:28:01 -0400

    2

--- truffle-config.js
+++ truffle-config.js
@@ -5,6 +5,13 @@ module.exports = {
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
+
+    // compilers: {
+    //   solc: {
+    //     version: "0.8.7",
+    //   },
+    // },
+  
     develop: {
       port: 8545
     },
@@ -14,11 +21,6 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
+  },
 
-    compilers: {
-      solc: {
-        version: "0.8.0",
-      },
-    },
-  }
 };
